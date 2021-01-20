e3bf074949dbf18b8e7402854e6232524247e365
kassner <kasser@cis-lmu.de>
2021-01-20 13:10:08 +0100

    database class

---
+++ bert_knn/label_database.py
@@ -0,0 +1,41 @@
+class LabelDB(object):
+    """Sqlite backed document storage.
+    Implements get_doc_text(doc_id).
+    """
+
+    def __init__(self, db_path=''):
+        self.path = db_path
+        self.connection = sqlite3.connect(self.path, check_same_thread=False)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.close()
+
+    def path(self):
+        """Return the path to the file that backs this database."""
+        return self.path
+
+    def close(self):
+        """Close the connection to the database."""
+        self.connection.close()
+
+    def get_instance_id(self):
+        """Fetch all ids of docs stored in the db."""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT instance_id FROM labels")
+        results = [r[0] for r in cursor.fetchall()]
+        cursor.close()
+        return results
+
+    def get_labels(self, instance_id):
+        """Fetch the raw text of the doc for 'doc_id'."""
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "SELECT label FROM labels WHERE instance_id = ?",
+            (instance_id,)
+        )
+        result = cursor.fetchone()
+        cursor.close()
+        return result if result is None else result[0]
